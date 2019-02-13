1d054da53bacb192275dc6af94d5d8dfd2ead19d
nqb <nqb@users.noreply.github.com>
2019-02-13 08:37:13 +0100

    use dedicated dir for tests

---
+++ test/integration/hello-doc-ng.install/default_test.rb
@@ -0,0 +1,16 @@
+# # encoding: utf-8
+
+# test installation of hello-doc-ng
+describe package('hello-doc-ng') do
+  it { should be_installed }
+end
+
+# test apache is running
+describe http('http://localhost/README.html') do
+  its('status') { should cmp 200 }
+end
+
+# test content in HTML page
+describe http('http://localhost/README.html') do
+  its('body') { should match /Zayme/ }
+end
